837d61a76b957708282b8c139a85fd8bde24e5aa
kwamecody <kwamecody@gmail.com>
2026-06-07 20:52:18 +0000

    connect client server to load user balance

--- server/src/routes/v1/ledger.routes.ts
+++ server/src/routes/v1/ledger.routes.ts
@@ -4,7 +4,7 @@ import { LedgerController } from '../../controllers/v1/ledger.controller';
 const ledgerRouter: Router = Router();
 
 ledgerRouter.post('/', LedgerController.create);
-(ledgerRouter.get('/balance/:accountId', LedgerController.getBalance),
-  ledgerRouter.get('/accounts', LedgerController.getAccounts));
+ledgerRouter.get('/balance/:accountId', LedgerController.getBalance)
+ledgerRouter.get('/accounts', LedgerController.getAccounts)
 
 export default ledgerRouter;
